f2329de646a6c2ecb95a202d864183765d7ab612
Shweta Kumari <shwetakri41299@gmail.com>
2020-05-18 02:25:37 +0530

    AR Major Project
    First Commmit

---
+++ floatingAR/Assets/cntDown.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cntDown : MonoBehaviour
+{
+    public static float currentTime = 0f, startTime = 30f;
+    [SerializeField]
+    Text countDownText;
+    
+
+    public Text lostText; 
+    public Button restartButton;
+    public Button quitButton;
+  
+    void Start()
+    {
+        currentTime = startTime;
+    }
+
+   
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        if (currentTime >= 0)
+        {
+            countDownText.text = currentTime.ToString("00");
+        }
+        else
+        {
+            countDownText.text = "00"; //currentTime.ToString("00");
+        }
+
+        countDownText.color = Color.green;
+
+        if (currentTime <= 5)
+        { countDownText.color = Color.red; countDownText.fontSize = 18; }
+
+        if (currentTime <= 0 && collisionScript.score<4)
+        {
+            lostText.gameObject.SetActive(true);
+            restartButton.gameObject.SetActive(true);
+            quitButton.gameObject.SetActive(true);
+            currentTime = 0;
+            //Application.Quit(); currentTime = 0; 
+        }
+    }
+}
---
+++ floatingAR/Assets/collisionScript.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class collisionScript : MonoBehaviour
+{
+    public AudioSource blast;
+    public Text winText;
+    public Button restartButton;
+    public Button quitButton;
+    public Text scoreText; public static float score = 0;
+
+    void Start()
+    {
+     blast = GetComponent<AudioSource>();
+
+    }
+
+    void Update()
+    {
+        score = 4 - GameObject.FindGameObjectsWithTag("Player").Length;
+        scoreText.text = score.ToString();
+        if (score == 4)
+        {
+            winText.gameObject.SetActive(true);
+            restartButton.gameObject.SetActive(true);
+            quitButton.gameObject.SetActive(true);
+            cntDown.currentTime = 0;
+        }
+        
+    }
+
+    
+
+    void OnTriggerEnter(Collider col)
+    {
+        GameObject explosion = Instantiate(Resources.Load("FlareMobile", typeof(GameObject))) as GameObject;
+       
+        blast.Play();
+        explosion.transform.position = transform.position;
+        Destroy(col.gameObject);
+        Destroy(explosion, 2);
+
+       /* score+=1;
+        scoreText.text = score.ToString();
+        
+
+
+        if (GameObject.FindGameObjectsWithTag("Player").Length == 0)
+        {
+            winText.gameObject.SetActive(true);
+            restartButton.gameObject.SetActive(true);
+          
+        }
+
+        if (score == 4)
+        {
+            winText.gameObject.SetActive(true);
+            restartButton.gameObject.SetActive(true);
+        }*/
+
+        Destroy(gameObject);
+
+
+    }
+
+}
